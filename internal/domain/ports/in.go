00d9c97dd226205ab4514d87a0951b0b48640e23
Igor Pestana <igor.pestana@greatcloudservices.com>
2023-10-25 10:57:23 -0300

    Ports

--- internal/domain/ports/in.go
+++ internal/domain/ports/in.go
@@ -6,14 +6,24 @@ import (
 )
 
 type CustomerService interface {
+	GetCustomerById(id string) (models.Customer, error)
 	GetCustomerByCPF(cpf string) (models.Customer, error)
 	CreateCustomer(customerDTO dto.CustomerDTO) error
 }
 
 type ProductService interface {
-	GetAllProducts() ([]models.Product, error)
-	GetProductsByCategory(category string) ([]models.Product, error)
+	GetAllProducts(pageParameters dto.PageParams) (dto.Page[models.Product], error)
+	GetProductsByCategory(pageParameters dto.PageParams, category string) (dto.Page[models.Product], error)
 	CreateProduct(productDTO dto.ProductDTO) error
 	UpdateProduct(id string, productDTO dto.ProductDTO) error
 	DeleteProduct(id string) error
+}
+
+type OrderService interface {
+	GetAllOrders(pageParameters dto.PageParams) (dto.Page[models.Order], error)
+	CreateOrder(orderDTO dto.OrderDTO) (string, error)
+}
+
+type PaymentService interface {
+	ProcessPayment(order models.Order) (string, error)
 }
--- internal/domain/ports/out.go
+++ internal/domain/ports/out.go
@@ -1,16 +1,34 @@
 package ports
 
-import "g37-lanchonete/internal/domain/models"
+import (
+	"g37-lanchonete/internal/domain/models"
+	"g37-lanchonete/internal/domain/services/dto"
+)
 
 type CustomerRepository interface {
-	SaveCustomer(customer models.Customer) error
+	FindCustomerById(id string) (models.Customer, error)
 	FindCustomerByCPF(cpf string) (models.Customer, error)
+	SaveCustomer(customer models.Customer) error
 }
 
 type ProductRepository interface {
-	FindAllProducts() ([]models.Product, error)
-	FindProductsByCategory(category string) ([]models.Product, error)
+	FindAllProducts(pageParams dto.PageParams) ([]models.Product, error)
+	FindProductsByCategory(pageParams dto.PageParams, category string) ([]models.Product, error)
 	SaveProduct(product models.Product) error
 	UpdateProduct(id uint, product models.Product) error
 	DeleteProduct(id uint) error
+}
+
+type OrderRepository interface {
+	FindAllOrders(pageParams dto.PageParams) ([]models.Order, error)
+	SaveOrder(order models.Order) error
+	UpdateOrder(id uint, order models.Order) error
+}
+
+type PaymentBroker interface {
+	GeneratePaymentQRCode(dto.PaymentQRCodeRequest) (dto.PaymentQRCodeResponse, error)
+}
+
+type PaymentOrderRepository interface {
+	SavePaymentOrder(models.PaymentOrder) error
 }
